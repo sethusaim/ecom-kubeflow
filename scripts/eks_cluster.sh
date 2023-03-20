2f0560acf584c6d6d4024cd61b7cb49d3e783dd5
sethusaim <sethusaim@gmail.com>
2023-03-20 15:49:23 +0530

    added commands to get kubeflow dashboard url

--- scripts/eks_cluster.sh
+++ scripts/eks_cluster.sh
@@ -1,3 +1,7 @@
 #!bin/bash
 
-eksctl create cluster --name kubeflow --version 1.22 --region us-east-1 --zones us-east-1a,us-east-1b --nodegroup-name kubeflow-nodes --node-type t2.large --nodes-min 2 --nodes-max 4 --with-oidc
+eksctl create cluster --name kubeflow --version 1.22 --region us-east-1 --zones us-east-1a,us-east-1b --nodegroup-name kubeflow-nodes --node-type t2.large --nodes-min 3 --nodes-max 4 --with-oidc
+
+kubectl patch svc istio-ingressgateway -n istio-system -p '{"spec":{"type":"LoadBalancer"}}'
+
+kubectl get svc istio-ingressgateway -n istio-system 
